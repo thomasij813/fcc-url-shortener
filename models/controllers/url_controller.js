790026cfb6a9212007a815cce6053d7c5a0e634b
Thomas Johnson <thomasij813@gmail.com>
2016-02-08 11:15:31 -0800

    Move logic of saving url into its own function for clarity

--- models/controllers/url_controller.js
+++ models/controllers/url_controller.js
@@ -1,27 +1,13 @@
 var Url = require('../url.js');
 var validUrl = require('valid-url');
+var http = require('http');
 
 exports.saveUrl = function(req, res) {
   var validatedUrl = validUrl.isWebUri(req.params.url);
-  if (!validatedUrl) {
+  if (!validatedUrl)
     res.json({message: req.params.url + ' is not a valid url'});
-  } else {
-    var url_doc = new Url({ url: validatedUrl });
-    url_doc.save(function(err, url) {
-      if (err && err.code === 11000) {
-        err = err.toJSON();
-        res.json({
-          original_url: err.op.url,
-          shortened_url: req.hostname + '/' + err.op._id
-        });
-      } else {
-        res.json({
-          original_url: url.url,
-          shortened_url: url._id
-        });
-      }
-    });
-  }
+  else
+    saveUrl(validatedUrl, req, res);
 };
 
 exports.getAll = function(req, res) {
@@ -57,3 +43,21 @@ exports.deleteAll = function(req, res) {
     }
   });
 };
+
+function saveUrl(url, req, res) {
+  var url_doc = new Url({ url: url});
+  url_doc.save(function(err, url) {
+    if (err && err.code === 11000) {
+      err = err.toJSON();
+      res.json({
+        original_url: err.op.url,
+        shortened_url: req.hostname + '/' + err.op._id
+      });
+    } else {
+      res.json({
+        original_url: url.url,
+        shortened_url: url._id
+      });
+    }
+  });
+}
